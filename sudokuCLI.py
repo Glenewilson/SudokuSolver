02eda4ee0c9ab68bbc8790b3881e4913465e3f28
Glen Wilson <glenedward@gmail.com>
2025-01-13 21:40:57 -0500

    changed log format, added runner configuration.

--- sudokuCLI.py
+++ sudokuCLI.py
@@ -84,16 +84,10 @@ def main():
             break
 
 logging.basicConfig(filename='SudokuSolver.log',
-                    format='%(asctime)s %(message)s',
+                    format='%(asctime)s %(levelname)s %(message)s',
                     filemode='w',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
-#logger.setLevel(logging.INFO)
-#handler = logging.StreamHandler()
-#handler.setLevel(logging.INFO)
-#formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-#handler.setFormatter(formatter)
-#logger.addHandler(handler)
 
 myGrid = SudokuV1.Grid()
                                     
